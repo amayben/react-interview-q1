a0a49fafffa7b5437a74a7b8374bc47fe5b6bad9
amayben <maybenalex@gmail.com>
2024-01-10 15:53:03 -0800

    Implemented basic APIs, committing what I have just to have something completed in time.

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useCallback, useEffect, useState} from 'react';
+import {getLocations, isNameValid} from './mock-api/apis';
 
 function App() {
+  const [nameValid, setNameValid] = useState(true);
+  const [locations, setLocations] = useState([]);
+
+  const checkName = useCallback(async event => {
+    const nameTaken = await isNameValid(event.target.value);
+    setNameValid(nameTaken);
+  }, [setNameValid]);
+
+  const getLocationsCallback = useCallback(async () => {
+    const locations = await getLocations();
+    setLocations(locations);
+  }, setLocations);
+
+  useEffect(() => {
+    getLocationsCallback();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <label>
+        <div>Name: <input onChange={checkName}/></div>
+        {!nameValid && <div>this name has already been taken</div>}
+      </label>
+      <label>
+        Location: 
+        <select>
+          {locations.map((location, index) => {
+            return <option id={index} value={index}>{location}</option>;
+          })}
+        </select>
+      </label>
+    </form>
   );
 }
 
